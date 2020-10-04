bbfb2b0f2582630176fd05237a5740f9fac518d3
rakeshdr543 <rakeshdr543@gmail.com>
2020-10-04 23:44:03 +0530

    Updates

---
+++ precache-manifest.6853369d4cf659ab1c47174f0d6b45a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be11efe467e95cf43a8198b722499a50",
+    "url": "/seriesapp/index.html"
+  },
+  {
+    "revision": "827236a6fb9a0cc807b9",
+    "url": "/seriesapp/static/css/main.f4a18ab1.chunk.css"
+  },
+  {
+    "revision": "cb8ded409574bc447c35",
+    "url": "/seriesapp/static/js/2.cf164db5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seriesapp/static/js/2.cf164db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827236a6fb9a0cc807b9",
+    "url": "/seriesapp/static/js/main.cfb95ffc.chunk.js"
+  },
+  {
+    "revision": "517d86292a67978ad8a3",
+    "url": "/seriesapp/static/js/runtime-main.b2cea80a.js"
+  }
+]);
